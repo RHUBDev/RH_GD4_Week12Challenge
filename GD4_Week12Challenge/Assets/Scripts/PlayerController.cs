facc88fe95d194197fdc672147faa063b5aaf7e9
RHUBDev <roberthendrygames@gmail.com>
2025-02-21 20:15:40 +0000

    Tried to fix most webbing bugs

--- GD4_Week12Challenge/Assets/Scripts/PlayerController.cs
+++ GD4_Week12Challenge/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,8 @@ using System.Collections;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
 using UnityEngine.SceneManagement;
+using UnityEngine.XR;
+using Unity.VisualScripting;
 
 public class PlayerController : MonoBehaviour
 {
@@ -48,6 +50,24 @@ public class PlayerController : MonoBehaviour
     private bool dead = false;
     private string levelName;
     [SerializeField] private Transform webpoint;
+    [SerializeField] private Animation anim;
+    private float webAnimTime = 0.5f;
+    private float webAnimTimer = 0.5f;
+    private Vector3 handRelativePos = Vector3.zero;
+    private Quaternion handRelativeRot = Quaternion.identity;
+    public Transform hand;
+    public Transform camParent;
+    private Vector3 webNorm = Vector3.zero;
+    private Quaternion handEndRot = Quaternion.identity;
+    public Transform webDummy;
+    Collider collision1;
+    Vector3 collision1Normal;
+    Collider currentCollision;
+    Vector3 currentCollisionNormal;
+    private bool stoppedWeb = false;
+    [SerializeField] private SkinnedMeshRenderer mesh;
+    private bool grounded2 = false;
+    private Vector3 webpos2 = Vector3.zero;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -72,6 +92,11 @@ public class PlayerController : MonoBehaviour
             Cursor.visible = true;
             Cursor.lockState = CursorLockMode.None;
         }
+
+        handRelativePos = hand.localPosition;
+        handRelativeRot = hand.localRotation;
+        mesh.enabled = false;
+        hand.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -97,31 +122,72 @@ public class PlayerController : MonoBehaviour
                         RaycastHit ray;
 
                         //cast a ray forwards in the camera direction
-                        bool ahit = Physics.Raycast(controller.transform.position, playerCamera.transform.forward, out ray, 1000f, layerMask, QueryTriggerInteraction.Ignore);
+                        bool ahit = Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out ray, 1000f, layerMask, QueryTriggerInteraction.Ignore);
 
                         if (ahit)
                         {
-                            //'webbed' is set to true after webbing is stopped by a collision or end of the web journey 
-                            webbed = false;
-
-                            //webJourney will be increased to 1 later, used for position Lerp
-                            webJourney = 0f;
-
-                            //start position of the web
-                            startPos = transform.position;//transform.position;
-
                             //end point of web
                             webpos = ray.point;
+                            webpos2 = webpos;
+                            RaycastHit ray2;
+                            if (ray.normal.y < 0.5)
+                            {
+                                //the web vector
+                                webVector = webpos - playerCamera.transform.position;
+                            }
+                            else
+                            {
+                                if (Physics.Raycast(controller.transform.position + Vector3.down * 1.2f, playerCamera.transform.forward, out ray2, 1000f, layerMask, QueryTriggerInteraction.Ignore))
+                                {
+                                    webpos2 = ray2.point;
+                                    webVector = webpos2 - (controller.transform.position + Vector3.down * 1.2f);
+                                }
+                            }
+                            Debug.Log("2.1");
 
-                            //the web vector
-                            webVector = webpos - transform.position;
+                            bool doit = false;
+                            if (currentCollision)
+                            {
+                                doit = Vector3.Dot(-currentCollisionNormal, webVector.normalized) < 0;
+                            }
+                            else
+                            {
+                                doit = true;
+                            }
+                                Debug.Log("Dot = " + Vector3.Dot(-currentCollisionNormal, webVector.normalized));
+                            if (doit)//Vector3.Dot(-currentCollisionNormal, webVector) < 0)
+                            {
+                                Debug.Log("2.2");
 
-                            //extra movement will be the added velocity after stopping webbing mid-web
-                            extraMovement = webVector.normalized * webSpeed;
+                                hand.SetParent(camParent);
+                                hand.localPosition = handRelativePos;
+                                hand.localRotation = handRelativeRot;
+                                //'webbed' is set to true after webbing is stopped by a collision or end of the web journey 
+                                webbed = false;
 
-                            //set webbing  boolean and set the web line object visible
-                            webbing = true;
-                            web.SetActive(true);
+                                mesh.enabled = true;
+                                hand.gameObject.SetActive(true);
+
+                                stoppedWeb = false;
+
+                                //webJourney will be increased to 1 later, used for position Lerp
+                                webJourney = 0f;
+
+                                //start position of the web
+                                startPos = transform.position;//transform.position;
+
+                                //extra movement will be the added velocity after stopping webbing mid-web
+                                extraMovement = webVector.normalized * webSpeed;
+
+                                //set webbing  boolean and set the web line object visible
+                                webbing = true;
+                                web.SetActive(true);
+
+                                webNorm = ray.normal;
+
+                                collision1 = ray.collider;
+                                collision1Normal = ray.normal;
+                            }
                         }
                     }
                     else
@@ -131,6 +197,9 @@ public class PlayerController : MonoBehaviour
                         //set all gravity Y direction speeds to 0
                         movementDirectionY = 0f;
                         moveDirection.y = 0f;
+
+                        mesh.enabled = false;
+                        hand.gameObject.SetActive(false);
 
                         //set extraMovement, I know we already called it higher up in the code though
                         extraMovement = webVector.normalized * webSpeed;
@@ -143,6 +212,10 @@ public class PlayerController : MonoBehaviour
 
                         //hide web object
                         web.SetActive(false);
+
+                        hand.SetParent(playerCamera.transform);
+                        hand.localPosition = handRelativePos;
+                        hand.localRotation = handRelativeRot;
                     }
                 }
 
@@ -156,13 +229,16 @@ public class PlayerController : MonoBehaviour
                 if (!webbing)
                 {
                     //and if grounded OR recently webbed (stuck to building)
-                    if (controller.isGrounded || webbed)
+                    if (controller.isGrounded || webbed || stoppedWeb || grounded2)
                     {
                         //set extra movement and y falling to 0
                         extraMovement = Vector3.zero;
 
                         movementDirectionY = 0f;
                         moveDirection.y = 0f;
+
+                        currentCollision = collision1;
+                        currentCollisionNormal = collision1Normal;
 
                         //I multiplied the moveDirection x and z here individually, as I thought it might multiply the gravity too otherwise
                         if (Input.GetKeyDown(KeyCode.LeftShift))
@@ -180,16 +256,31 @@ public class PlayerController : MonoBehaviour
                         if (horizontalInput != 0 || verticalInput != 0)
                         {
                             webbed = false;
+                            mesh.enabled = false;
+                            hand.gameObject.SetActive(false);
                         }
 
                         //Jumping
                         if (Input.GetButtonDown("Jump"))
                         {
                             webbed = false;
+                            stoppedWeb = false;
+                            grounded2 = false;
+                            currentCollision = null;
                             moveDirection.y = jumpForce;
                         }
+                        webAnimTimer = 0;
+
+                        anim["Armature|palm"].time = anim["Armature|palm"].length;
+
+                        if (webbed)
+                        {
+                            hand.position = webDummy.position;
+                            hand.rotation = webDummy.rotation;
+                        }
+                        
                     }
-                    else if (!webbed)
+                    else if (!webbed && !stoppedWeb)
                     {
                         //if not grounded or webbed
 
@@ -252,17 +343,27 @@ public class PlayerController : MonoBehaviour
                 {
                     //if we ARE webbing currently
 
+                    Debug.Log("webpos = " + webpos);
                     //set the web object position to mid-way, scale to the length of the web vector, and rotation to LookAt the webpos (web destination)
                     web.transform.position = webpoint.position + (webpos - webpoint.position) / 2;
                     web.transform.localScale = new Vector3(0.014f, 0.014f, (webpos - webpoint.position).magnitude);
                     web.transform.LookAt(webpos);
 
-                    //increase webJourney to 1, so it takes the time to travel length of webVector at webSpeed
-                    webJourney += (Time.deltaTime * webSpeed) / webVector.magnitude;
+                    if (webAnimTimer < webAnimTime)
+                    {
+                        webAnimTimer += Time.deltaTime;
+                        anim.Play("Armature|palm");
+                    }
+                    else
+                    {
+                        anim.Stop();
+                    }
+                        //increase webJourney to 1, so it takes the time to travel length of webVector at webSpeed
+                        webJourney += (Time.deltaTime * webSpeed) / webVector.magnitude;
                     if (webJourney < 1)
                     {
                         //move the player controller with Lerp, from startPos to the destination
-                        controller.transform.position = Vector3.Lerp(startPos, webpos, webJourney);
+                        controller.transform.position = Vector3.Lerp(startPos, startPos + webVector, webJourney);
                     }
                     else
                     {
@@ -273,6 +374,31 @@ public class PlayerController : MonoBehaviour
                         extraMovement = Vector3.zero;
                         webbing = false;
                         webbed = true;
+
+                        currentCollision = collision1;
+                        currentCollisionNormal = collision1Normal;
+
+                        if (webNorm.y < 0.5)
+                        {
+                            Debug.Log("0.1");
+                            StartCoroutine(WebbedHand());
+                        }
+                        else
+                        {
+                            webbed = false;
+                            stoppedWeb = true;
+                            mesh.enabled = false;
+                            hand.gameObject.SetActive(false);
+                            hand.localPosition = handRelativePos;
+                            hand.localRotation = handRelativeRot;
+
+                            RaycastHit ray;
+                            if (Physics.Raycast(playerCamera.transform.position + Vector3.down * 0.1f, Vector3.down, out ray, 2.5f, layerMask, QueryTriggerInteraction.Ignore))
+                            {
+                                Debug.Log("3");
+                                controller.transform.position = ray.point + Vector3.up * 1.4f;
+                            }
+                        }
                     }
                 }
 
@@ -302,14 +428,46 @@ public class PlayerController : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
+        bool doit;
+        if (currentCollision)
+        {
+            doit = Vector3.Dot(-currentCollisionNormal, webVector) >= 0;
+        }
+        else
+        {
+            doit = true;
+        }
         //if we hit something while webbing, stop webbing (it could be the destination object; we will never actually reach the destination because the Lerp goes all the way to the centre of the characterController at the moment)
-        if (webbing)
+        if (webbing && doit)//((Vector3.Dot(-currentCollisionNormal, webVector) >= 0)))// || !(Vector3.Dot(-currentCollisionNormal, webVector) >= 0 && currentCollision == collision.collider)))
         {
             moveDirection.y = 0f;
             web.SetActive(false);
             extraMovement = Vector3.zero;
             webbing = false;
-            webbed = true;
+
+            hand.SetParent(playerCamera.transform);
+
+            collision1 = collision.collider;
+            collision1Normal = collision.contacts[0].normal;
+
+            //if (!controller.isGrounded)
+            //{
+            if (collision.contacts[0].normal.y<0.5)
+            {
+                Debug.Log("0.2");
+                webbed = true;
+                //collision1 = collision.collider;
+                StartCoroutine(WebbedHand());
+            }
+            else
+            {
+                stoppedWeb = true;
+                mesh.enabled = false;
+                hand.gameObject.SetActive(false);
+                hand.localPosition = handRelativePos;
+                hand.localRotation = handRelativeRot;
+                controller.transform.position = collision.contacts[0].point + Vector3.up * 1.4f;
+            }
         }
     }
 
@@ -377,5 +535,101 @@ public class PlayerController : MonoBehaviour
         {
             Time.timeScale = 0f;
         }
+    }
+
+    IEnumerator WebbedHand()
+    {
+        Vector3 handStartPos = hand.position;
+        Vector3 handEndPos = webpos;
+        Vector3 newNorm = webNorm;
+        /*if (collision != null)
+        {
+            handEndPos = collision.contacts[0].point;//collision.collider.ClosestPoint(hand.position);
+            newNorm = collision.contacts[0].normal;
+        }*/
+
+        RaycastHit ray;
+        if (Physics.Raycast(playerCamera.transform.position, -newNorm, out ray, 1f, layerMask, QueryTriggerInteraction.Ignore))
+        {
+            Debug.Log("1.1");
+            newNorm = ray.normal;
+            handEndPos = ray.point;
+        }
+
+        /*if (collision1 != null)
+        {
+            Debug.Log("1");
+            handEndPos = collision1.ClosestPoint(playerCamera.transform.position);
+        }
+        Debug.Log("1.1");
+        */
+        float diffMag = (handEndPos - playerCamera.transform.position).magnitude;
+        if (diffMag < 0.6f)
+        {
+            Debug.Log("1.2");
+            controller.transform.position = controller.transform.position - (newNorm * (diffMag - 0.6f));
+        }
+
+        /*RaycastHit ray;
+        
+        //cast a ray forwards in the camera direction
+        if (Physics.Raycast(playerCamera.transform.position, (handEndPos - playerCamera.transform.position).normalized, out ray, 1000f, layerMask, QueryTriggerInteraction.Ignore))
+        {
+            newNorm = ray.normal;
+        }*/
+
+        //if(diffMag < 0.5f)
+        //{
+        //    controller.transform.position = controller.transform.position - (newNorm * (diffMag - 0.5f));
+        //}
+
+        Quaternion handStartRot = hand.rotation;
+        webDummy.rotation = Quaternion.FromToRotation(-transform.up, -newNorm);
+        webDummy.position = handEndPos;
+        //webDummy.rotation = Quaternion.LookRotation(-webVector, newNorm);
+        //webDummy.up = newNorm;
+        //float angle = Mathf.Acos(Vector3.Dot(-webVector, -handEndRot.)) * Mathf.Rad2Deg;
+        //handEndRot *= Quaternion.Euler(-newNorm * angle);
+        float angle = Vector3.SignedAngle(-webVector, webDummy.forward, -newNorm);
+        webDummy.rotation *= Quaternion.Euler(0, angle, 0);
+        handEndRot = webDummy.rotation;
+
+        float t = 0;
+        while (t < 1)
+        {
+            t += Time.deltaTime * 3f;
+            hand.position = Vector3.Lerp(handStartPos, handEndPos, t);
+            hand.rotation = Quaternion.Lerp(handStartRot, handEndRot, t);
+            yield return null;
+        }
+        hand.position = handEndPos;
+        hand.rotation = handEndRot;
+        anim["Armature|palm"].time = anim["Armature|palm"].length;
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+
+        collision1 = collision.collider;
+        collision1Normal = collision.contacts[0].normal;
+        currentCollision = collision.collider;
+        currentCollisionNormal = collision.contacts[0].normal;
+        Debug.Log("currentCollision = " + currentCollision.name);
+        grounded2 = collision.contacts[0].normal.y >= 0.5f;
+        //Debug.Log("currentCollisionNormal = " + currentCollisionNormal);
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (currentCollision == collision.collider)
+        {
+            currentCollision = null;
+        }
+        grounded2 = false;
+
+        /*if (collision1 == collision.collider)
+        {
+            collision1 = null;
+        }*/
     }
 }
